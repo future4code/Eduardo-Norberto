7e5d03a19d6ace09cc264a0409b828704c8ed1e8
eduardomendesnorberto <eduardomendesnorberto@outlook.com>
2019-11-19 17:16:11 -0300

    FimDoExerciciosJS

--- semana4/aula2-objetos-string/exercicios-js/index.js
+++ semana4/aula2-objetos-string/exercicios-js/index.js
@@ -11,13 +11,41 @@
  *  
  */
 /*******3 ********/
-function criaRetangulo(lado1, lado2){
-    const retatangulo = {
-        largura: lado1,
-        altura: lado2,
-        perimetro: 2 * (lado1 + lado2),
-        area: (lado1 * lado2)
-    }
-    return retatangulo
+// function criaRetangulo(lado1, lado2){
+//     const retangulo = {
+//         largura: lado1,
+//         altura: lado2,
+//         perimetro: 2 * (lado1 + lado2),
+//         area: (lado1 * lado2),
+//     }
+//     return retangulo.altura
+// }
+// criaRetangulo(5, 2)
+
+/******4 ********/
+// const pessoa = {
+//     nome: 'Ana',
+//     idade: 25,
+//     email: 'ana@email.com',
+//     endereco: 'Rua 2'
+// }
+// console.log(pessoa)
+
+
+// function anonimizarPessoa(){
+//     const novaPessoa = {
+//         ...pessoa,
+//         nome: 'Anonimo'
+//     }
+//     console.log(novaPessoa)
+// }
+
+/******5 *****/
+
+const filme = {
+    titulo: 'O Labirinto do Fauno',
+    ano: 2006,
+    diretor: 'Guillermo del Toro',
+    atores: ['Ivana Baquero', 'Doug Jones', 'Sergi López']
 }
- criaRetangulo(5, 2)
+console.log("Venha assistir ao filme " + filme.titulo + ","+"\n" + "de " + filme.ano + " ,dirigido por " + filme.diretor  + " e estrelado por " + "\n"+ filme.atores + ".")
