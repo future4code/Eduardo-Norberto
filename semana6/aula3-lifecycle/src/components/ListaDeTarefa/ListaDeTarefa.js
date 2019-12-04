bcf5de8cdfdc2306955378f28945c6d8bcb7832b
eduardomendesnorberto <eduardomendesnorberto@outlook.com>
2019-12-04 16:57:08 -0300

    Envio parcial da Atividade

---
+++ semana6/aula3-lifecycle/src/components/ListaDeTarefa/ListaDeTarefa.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 300px;
+    margin: 0 auto;
+`;
+
+const ListaFormato = styled.ul `
+    list-style: outside;
+    text-align: left;
+`;
+
+const ItemPendente = styled.li `
+    text-decoration: none;
+`;
+const ItemCompleto = styled.li `
+    text-decoration: line-through;
+`;
+
+
+class ListaDeTarefa extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            arrayDeTarefa: [],
+            descTarefa: '',
+            filtroDeTarefa: ''
+        };
+    }
+
+    controleTarefa = event => {
+        this.setState ({
+            descTarefa: event.target.value
+        });
+    };
+
+    controleFiltro = event => {
+        this.setState({
+            filtroDeTarefa: event.target.value
+        });
+    };
+
+    adicionaNovaTarefa = () => {
+        const novaTarefa = {
+            id: Date.now(),
+            tarefa: this.state.descTarefa,
+            completa: false
+        }
+        let arrayCopiaTarefa = [...this.state.arrayDeTarefa]
+        arrayCopiaTarefa.push(novaTarefa)
+        this.setState ({
+            arrayDeTarefa: arrayCopiaTarefa
+        })
+    }
+
+    onClickCheckup = id => {
+        const varificaTarefa = this.state.arrayDeTarefa.map(descricao => {
+          if (descricao.id === id) {
+            return {
+              ...descricao,
+              completa: !descricao.completa
+            };
+          }
+          return descricao;
+        });
+        this.setState({ 
+            arrayDeTarefa: varificaTarefa 
+        });
+      };
+
+    render () {
+        return (
+            <div>
+                <div>
+                    <h1>Lista de Tarefas</h1>
+                    <input value={this.state.descTarefa} onChange={this.controleTarefa}/>
+                    <button onClick={this.adicionaNovaTarefa}>Adicionar</button>
+                    <p>Filtro</p>
+                        <select value={this.state.filtroDeTarefa} onChange={this.controleFiltro}>
+                            <option value="nenhum">Nenhum</option>
+                            <option value="pendentes">Pendentes</option>
+                            <option value="completas">Completas</option>
+                        </select>
+                </div>
+                <Container>
+
+                    {this.state.arrayDeTarefa.filter(descricao => {
+                            if (this.state.filtroDeTarefa === 'pendentes'){
+                                return !descricao.completa;
+                            }else if (this.state.filtroDeTarefa === 'completas'){
+                                return descricao.completa;
+                            }else{
+                                return false;
+                            }
+                    })}
+                
+                    {this.state.arrayDeTarefa.map(descricao =>{
+                        if (!descricao.completa){
+                            return (
+                                
+                                <ListaFormato>
+                                    <ItemPendente onClick={() => this.onClickCheckup(descricao.id)}>{descricao.tarefa}{console.log(descricao)}</ItemPendente>
+                                </ListaFormato>
+                                
+                            )
+                        }else{
+                            return (
+                               
+                                <ListaFormato>
+                                    <ItemCompleto onClick={() => this.onClickCheckup(descricao.id)}>{descricao.tarefa}{console.log(descricao)}</ItemCompleto>
+                                </ListaFormato>
+            
+
+                            )
+                        } 
+                    })}
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default ListaDeTarefa;  
