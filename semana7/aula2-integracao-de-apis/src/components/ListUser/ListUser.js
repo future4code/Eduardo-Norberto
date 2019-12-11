666f7e184a572378dddf66fd0b52ced567bf4598
eduardomendesnorberto <eduardomendesnorberto@outlook.com>
2019-12-11 13:08:33 -0300

    Vesao final

--- semana7/aula2-integracao-de-apis/src/components/ListUser/ListUser.js
+++ semana7/aula2-integracao-de-apis/src/components/ListUser/ListUser.js
@@ -14,6 +14,17 @@ function ListUser (props){
     return(
         <ListArea>
             name:{props.dadosUsuario.name}
+            {/* <UserList>
+        <h2>Usuários Cadastrados:</h2>
+        {this.props.users.map(user => (
+          <UserListItem key={user.id}>
+            {user.name}{" "}
+            <DeleteButton onClick={() => this.props.onDeleteUser(user.id)}>
+              X
+            </DeleteButton>
+          </UserListItem>
+        ))}
+      </UserList> */}
         </ListArea>
     );
 }
